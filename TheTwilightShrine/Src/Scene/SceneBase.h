92f9e2c883d979e317d4bbe9c0e91082ce1522fa
TAMURASOUSUKE <kokotomo.88934@gmail.com>
2026-09-16 07:29:45 +0900

    SceneBaseの定義

--- TheTwilightShrine/Src/Scene/SceneBase.h
+++ TheTwilightShrine/Src/Scene/SceneBase.h
@@ -1,1 +1,20 @@
 #pragma once
+
+// 各Sceneの基底クラスとなるクラス
+class SceneBase
+{
+public:
+	// デストラクタvirtualにする
+	virtual ~SceneBase() = default;
+
+	// シーンに入った際の処理
+	virtual void Initialize() = 0;
+	// シーン更新中の処理
+	virtual void Update() = 0;
+	// シーンを出るときの処理
+	virtual void Terminate() = 0;
+
+	// シーンを描画する処理
+	virtual void Draw() = 0;
+
+};
