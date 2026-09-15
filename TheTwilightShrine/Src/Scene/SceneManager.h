7209af242a407ac79a31038693d08efa0b02b9a5
TAMURASOUSUKE <kokotomo.88934@gmail.com>
2026-09-16 07:51:11 +0900

    ポリモーフィズムの主体となる関数の作成

--- TheTwilightShrine/Src/Scene/SceneManager.h
+++ TheTwilightShrine/Src/Scene/SceneManager.h
@@ -16,14 +16,16 @@ public:
 		return instance;
 	}
 
-	// コピー代入禁止
-	SceneManager(const SceneManager& _other) = delete;
-	SceneManager& operator=(const SceneManager& _other) = default;
-
 	// シーン登録関数
 	void Register(const std::unique_ptr<SceneBase>& _scene);
 	// シーン変更関数(true = 成功)
 	bool ChangeScene(SceneType _nextScene);
+
+	// シーンの更新
+	void Update();
+
+	// シーンの描画
+	void Draw();
 
 
 private:
