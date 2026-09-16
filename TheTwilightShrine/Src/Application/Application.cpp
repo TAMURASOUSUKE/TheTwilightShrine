86c56a27d16f0aa3f52d828899248e27f8edff4d
TAMURASOUSUKE <kokotomo.88934@gmail.com>
2026-09-16 14:24:13 +0900

    ライブラリ初期化失敗時に終了するように変更

--- TheTwilightShrine/Src/Application/Application.cpp
+++ TheTwilightShrine/Src/Application/Application.cpp
@@ -7,7 +7,11 @@ Application::Application()
 	// ライブラリ初期化
 	constexpr int WINDOW_WIDTH{ 1920 }; // 画面横幅
 	constexpr int WINDOW_HEIGHT{ 1080 }; // 画面縦幅
-	if (!TSLib::Initialize(L"黄昏の社", WINDOW_WIDTH, WINDOW_HEIGHT)) isInitialized = false;
+	if (!TSLib::Initialize(L"黄昏の社", WINDOW_WIDTH, WINDOW_HEIGHT))
+	{
+		isInitialized = false;
+		return;
+	}
 	SceneManager::Instance().ChangeScene(SceneType::Title); // とりあえず最初はタイトルから
 	isInitialized = true; // ここまで来たら成功とする
 }
