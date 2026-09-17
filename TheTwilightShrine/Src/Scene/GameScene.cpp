c7bd53a6bc535d273a8aa4cb4b22d0954c7b7454
TAMURASOUSUKE <kokotomo.88934@gmail.com>
2026-09-17 13:42:58 +0900

    LowEnemyの正常性確認

--- TheTwilightShrine/Src/Scene/GameScene.cpp
+++ TheTwilightShrine/Src/Scene/GameScene.cpp
@@ -9,10 +9,13 @@ void GameScene::Initialize()
 	light.directional.direction = { 10.0f, -10.0f, 0.0f };
 
 	player = Gfx::LoadModel("Res/3DModel/Player/Player.glb");
-	lowEnemy = Gfx::LoadModel()
+	lowEnemy = Gfx::LoadModel("Res/3DModel/LowEnemy/LowEnemy.glb");
 	playerInstance = Gfx::CreateAnimInstance(player);
-	playerTransform.SetPosition(Vector3::Zero);
+	lowEnemyInstance = Gfx::CreateAnimInstance(lowEnemy);
+	playerTransform.SetPosition({-20.0f, 0.0f, 0.0f});
+	lowEnemyTransform.SetPosition(Vector3::Zero);
 	Gfx::PlayAnim(playerInstance, playerClipIndex, true);
+	Gfx::PlayAnim(lowEnemyInstance, lowEnemyClipIndex, true);
 	DEBUG_LOG("GameSceneのInitializeを通りました!\n");
 }
 
@@ -32,6 +35,14 @@ void GameScene::Update()
 		playerClipIndex = std::max(0, playerClipIndex);
 		if (Input::IsKeyPushed(KeyCode::Button::P)) Gfx::CrossFadeAnim(playerInstance, playerClipIndex, 0.5f, true);
 		Gfx::UpdateAnim(playerInstance, Time::DeltaTime());
+	}
+	if (lowEnemyInstance.IsValid())
+	{
+		if (Input::IsKeyPushed(KeyCode::Button::D3)) lowEnemyClipIndex--;
+		if (Input::IsKeyPushed(KeyCode::Button::D4)) lowEnemyClipIndex++;
+		lowEnemyClipIndex = std::max(0, lowEnemyClipIndex);
+		if (Input::IsKeyPushed(KeyCode::Button::E)) Gfx::CrossFadeAnim(lowEnemyInstance, lowEnemyClipIndex, 0.5f, true);
+		Gfx::UpdateAnim(lowEnemyInstance, Time::DeltaTime());
 	}
 
 #endif // _DEBUG
@@ -56,8 +67,11 @@ void GameScene::Terminate()
 void GameScene::Draw()
 {
 	Gfx::DrawAnimatedModel(playerInstance, playerTransform);
+	Gfx::DrawAnimatedModel(lowEnemyInstance, lowEnemyTransform);
 
 	std::string playerClipIndexStr{ std::format("PlayerClipIndex : {}", playerClipIndex)};
+	std::string lowEnemyClipIndexStr{ std::format("LowEnemyClipIndex : {}", lowEnemyClipIndex)};
 	Gfx::DrawString("GameScene", Vector2::Zero);
 	Gfx::DrawString(playerClipIndexStr.c_str(), {0.0f, 20.0f});
+	Gfx::DrawString(lowEnemyClipIndexStr.c_str(), {0.0f, 40.0f});
 }
