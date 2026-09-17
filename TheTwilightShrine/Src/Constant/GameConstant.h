0ea8551bf3b88ca9d50b4909ee7aa87732b963aa
TAMURASOUSUKE <kokotomo.88934@gmail.com>
2026-09-18 01:26:58 +0900

    Facotry完成

--- TheTwilightShrine/Src/Constant/GameConstant.h
+++ TheTwilightShrine/Src/Constant/GameConstant.h
@@ -1,6 +1,6 @@
 #pragma once
 // ゲーム中に関係する定数や構造体、列挙体を定義する
-enum ObjectID
+enum class ObjectID
 {
 	None,
 	Player,
--- TheTwilightShrine/Src/Object/ObjectFactory.cpp
+++ TheTwilightShrine/Src/Object/ObjectFactory.cpp
@@ -1,20 +1,27 @@
+#include <memory>
+#include <utility>
 #include "ObjectManager.h"
 #include "ObjectFactory.h"
 
 // ここから具体ヘッダをインクルード
-#include ""
+#include "Player.h"
+#include "LowEnemy.h"
+#include "Boss.h"
 
 void ObjectFactory::CreatePlayer()
 {
-
+	std::unique_ptr<ObjectBase> player{ std::make_unique<Player>() }; // player生成
+	ObjectManager::Instance().Register(std::move(player)); // Managerに登録
 }
 
 void ObjectFactory::CreateLowEnemy()
 {
-
+	std::unique_ptr<ObjectBase> lowEnemy{ std::make_unique<LowEnemy>() }; // 雑魚敵生成
+	ObjectManager::Instance().Register(std::move(lowEnemy)); // Managerに登録
 }
 
 void ObjectFactory::CreateBoss()
 {
-
+	std::unique_ptr<ObjectBase> boss{ std::make_unique<Boss>() }; // boss生成
+	ObjectManager::Instance().Register(std::move(boss)); // Managerに登録
 }
